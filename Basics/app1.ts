d771be3e1206d8a9fef204b6cc553b286722814f
Srutip04 <subuhishruti75@gmail.com>
2023-03-30 04:30:37 +0530

    arrays types

--- Basics/app1.ts
+++ Basics/app1.ts
@@ -1,9 +1,14 @@
-const person: {
-    name: string;
-    age: number;
-} = {
+const person = {
   name: "shruti",
   age: 30,
+  hobbies: ["sports", "cooking"],
 };
 
+let favAct: string[];
+favAct = ["sports"];
+
 console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby);
+}
