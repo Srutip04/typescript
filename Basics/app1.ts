d2f43853d5c5d8b9425263c92f37f6f79839833e
Srutip04 <subuhishruti75@gmail.com>
2023-03-30 04:42:56 +0530

    enum type

--- Basics/app1.ts
+++ Basics/app1.ts
@@ -1,11 +1,24 @@
+// const person = {
+//   name: "shruti",
+//   age: 30,
+//   hobbies: ["sports", "cooking"],
+//   role: [2, "author"],
+// };
+
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUHTOR = 2;
+
+enum Role { ADMIN=4, READ_ONLY, AUHTOR };
+
 const person = {
   name: "shruti",
   age: 30,
   hobbies: ["sports", "cooking"],
-  role: [2, "author"],
+  role: Role.ADMIN,
 };
 
-person.role.push('admin');
+// person.role.push('admin');
 // person.role[1] = 10;
 
 let favAct: string[];
@@ -15,4 +28,8 @@ console.log(person.name);
 
 for (const hobby of person.hobbies) {
   console.log(hobby);
+}
+
+if (person.role == Role.AUHTOR) {
+    console.log('isauth'); 
 }
