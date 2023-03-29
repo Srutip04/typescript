88a86cb3da48ca3d9623321cc4525d2c5b910fc7
Srutip04 <subuhishruti75@gmail.com>
2023-03-29 23:40:20 +0530

    number,strings & Booleans

--- Basics/app.js
+++ Basics/app.js
@@ -1,8 +1,17 @@
 console.log("COde goes here");
-function add(n1, n2) {
-    return n1 + n2;
+function add(n1, n2, showRes, phrase) {
+    console.log(typeof num1);
+    var res = n1 + n2;
+    if (showRes) {
+        console.log(phrase + res);
+    }
+    else {
+        return res;
+    }
 }
 var num1 = 3;
 var num2 = 4.6;
-var result = add(num1, num2);
+var printRes = true;
+var resPhrase = "Result is: ";
+var result = add(num1, num2, printRes, resPhrase);
 console.log(result);
--- Basics/app.ts
+++ Basics/app.ts
@@ -1,12 +1,19 @@
 console.log("COde goes here");
 
-function add(n1: number, n2: number) {
-    return n1 + n2;
+function add(n1: number, n2: number, showRes: boolean, phrase: string) {
+  console.log(typeof num1);
+  const res = n1 + n2;
+  if (showRes) {
+    console.log(phrase + res);
+  } else {
+    return res;
+  }
 }
 
 const num1 = 3;
 const num2 = 4.6;
+const printRes = true;
+const resPhrase = "Result is: ";
 
-const result = add(num1, num2);
+const result = add(num1, num2, printRes, resPhrase);
 console.log(result);
-
