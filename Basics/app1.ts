2a40c8404fe93fa75723624c993b4f9cd19f0885
Srutip04 <subuhishruti75@gmail.com>
2023-03-30 04:35:26 +0530

    working with tuples

--- Basics/app1.ts
+++ Basics/app1.ts
@@ -2,7 +2,11 @@ const person = {
   name: "shruti",
   age: 30,
   hobbies: ["sports", "cooking"],
+  role: [2, "author"],
 };
+
+person.role.push('admin');
+// person.role[1] = 10;
 
 let favAct: string[];
 favAct = ["sports"];
