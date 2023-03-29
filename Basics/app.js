fc92281e5595bc77975260ef694b0851e0c95f1f
Srutip04 <subuhishruti75@gmail.com>
2023-03-29 23:29:09 +0530

    using types

--- Basics/app.js
+++ Basics/app.js
@@ -1,1 +1,8 @@
 console.log("COde goes here");
+function add(n1, n2) {
+    return n1 + n2;
+}
+var num1 = 3;
+var num2 = 4.6;
+var result = add(num1, num2);
+console.log(result);
--- Basics/app.ts
+++ Basics/app.ts
@@ -1,1 +1,12 @@
 console.log("COde goes here");
+
+function add(n1: number, n2: number) {
+    return n1 + n2;
+}
+
+const num1 = 3;
+const num2 = 4.6;
+
+const result = add(num1, num2);
+console.log(result);
+
